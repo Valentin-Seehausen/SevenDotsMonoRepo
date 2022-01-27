b8ac54bd1461ef4fa992757eaf7283e1f1b67348
Valentin Seehausen <valentin.seehausen@gmail.com>
2022-01-27 15:29:18 +0100

    add network chainId check

--- vitesse/src/stores/wallet.ts
+++ vitesse/src/stores/wallet.ts
@@ -6,11 +6,37 @@ export const useWalletStore = defineStore('wallet', () => {
   const isConnected = ref(false)
   const isMetaMaskInstalled = ref(false)
   const signer = ref<ethers.providers.JsonRpcSigner>()
+  const chainId = ref()
   const account = ref('')
 
   const checkMetaMask = async() => {
     if (typeof window == 'undefined') return
     isMetaMaskInstalled.value = Boolean(window && window.ethereum && window.ethereum.isMetaMask)
+  }
+
+  const changeNetwork = async() => {
+    try {
+      window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x89' }],
+      })
+    }
+    catch (e) {
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [{
+          chainId: '0x89', // 137
+          chainName: 'Matic(Polygon) Mainnet',
+          nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+          rpcUrls: ['https://polygon-rpc.com'],
+          blockExplorerUrls: ['https://www.polygonscan.com/'],
+        }],
+      })
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x89' }],
+      })
+    }
   }
 
   const checkConnection = async() => {
@@ -19,14 +45,19 @@ export const useWalletStore = defineStore('wallet', () => {
       method: 'eth_accounts',
     })
     if (_accounts.length > 0) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      signer.value = provider.getSigner()
+      const _provider = new ethers.providers.Web3Provider(window.ethereum)
+      const _signer = _provider.getSigner()
+      signer.value = _signer
+      chainId.value = await _signer.getChainId()
       isConnected.value = true
       account.value = _accounts[0]
+      if (chainId.value !== 137)
+        changeNetwork()
     }
   }
 
   const getSigner = () => {
+    if (!isConnected.value) return
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     return provider.getSigner()
   }
