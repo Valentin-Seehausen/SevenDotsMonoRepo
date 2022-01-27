e48cc1b7ecb5d93e72b3df2aae3743c124a7700e
Valentin Seehausen <valentin.seehausen@gmail.com>
2022-01-27 12:52:11 +0100

    style wallet button

--- vitesse/src/components/WalletButton.vue
+++ vitesse/src/components/WalletButton.vue
@@ -16,7 +16,7 @@ const onClick = () => {
 </script>
 
 <template>
-  <button class="w-48 overflow-hidden btn hover:btn-dark border-white" @click="onClick">
+  <button class="w-48 overflow-hidden btn" @click="onClick">
     <p v-if="!wallet.isMetaMaskInstalled">
       {{ t("wallet.no-metamask") }}
     </p>
