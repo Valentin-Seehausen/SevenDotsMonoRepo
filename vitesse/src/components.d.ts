be4d6fe3440430d2c567f302e3a88f7630c392e2
Valentin Seehausen <valentin.seehausen@gmail.com>
2022-02-09 21:10:25 +0100

    change vite setting

--- vitesse/src/components.d.ts
+++ vitesse/src/components.d.ts
@@ -6,10 +6,17 @@ declare module 'vue' {
   export interface GlobalComponents {
     ApprovalWarner: typeof import('./components/ApprovalWarner.vue')['default']
     Auction: typeof import('./components/Auction.vue')['default']
+    CarbonClose: typeof import('~icons/carbon/close')['default']
+    CarbonMoon: typeof import('~icons/carbon/moon')['default']
+    CarbonSun: typeof import('~icons/carbon/sun')['default']
     ChainWarner: typeof import('./components/ChainWarner.vue')['default']
     Counter: typeof import('./components/Counter.vue')['default']
+    'Gg:arrowsExchangeV': typeof import('~icons/gg/arrows-exchange-v')['default']
     NavBar: typeof import('./components/NavBar.vue')['default']
     README: typeof import('./components/README.md')['default']
+    'SimpleIcons:discord': typeof import('~icons/simple-icons/discord')['default']
+    'SimpleIcons:opensea': typeof import('~icons/simple-icons/opensea')['default']
+    'SimpleIcons:twitter': typeof import('~icons/simple-icons/twitter')['default']
     Token: typeof import('./components/Token.vue')['default']
     WalletButton: typeof import('./components/WalletButton.vue')['default']
   }
